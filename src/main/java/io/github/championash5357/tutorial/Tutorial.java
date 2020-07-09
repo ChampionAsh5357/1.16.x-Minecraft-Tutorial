6f6042bae4f1a9daf2b526f312e69799efec0435
ChampionAsh5357 <championash5357@gmail.com>
2020-07-09 00:21:01 -0400

    Port to 32.0.61 And Armor Fix

---
+++ src/main/java/io/github/championash5357/tutorial/Tutorial.java
@@ -0,0 +1,38 @@
+package io.github.championash5357.tutorial;
+
+import io.github.championash5357.tutorial.client.proxy.ClientProxy;
+import io.github.championash5357.tutorial.init.TutorialItems;
+import io.github.championash5357.tutorial.proxy.IProxy;
+import io.github.championash5357.tutorial.server.proxy.ServerProxy;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Tutorial.ID)
+public class Tutorial {
+
+	public static final String ID = "tutorial";
+	
+	public static final IProxy PROXY = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> ServerProxy::new);
+	
+	public Tutorial() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus(),
+				forgeEventBus = MinecraftForge.EVENT_BUS;
+		
+		//Basic Setup
+		PROXY.setup(modEventBus, forgeEventBus);
+		addRegistries(modEventBus);
+		
+		//Mod Event Bus Listeners
+		modEventBus.addListener(this::commonSetup);
+	}
+	
+	private void commonSetup(final FMLCommonSetupEvent event) {}
+	
+	private void addRegistries(final IEventBus modEventBus) {
+		TutorialItems.ITEMS.register(modEventBus);
+	}
+}
---
+++ src/main/java/io/github/championash5357/tutorial/server/proxy/ServerProxy.java
@@ -0,0 +1,10 @@
+package io.github.championash5357.tutorial.server.proxy;
+
+import io.github.championash5357.tutorial.proxy.IProxy;
+import net.minecraftforge.eventbus.api.IEventBus;
+
+public class ServerProxy implements IProxy {
+
+	@Override
+	public void setup(IEventBus modEventBus, IEventBus forgeEventBus) {}
+}
