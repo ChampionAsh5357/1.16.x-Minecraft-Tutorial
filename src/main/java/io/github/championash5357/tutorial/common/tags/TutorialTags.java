034463833b4803e46c4bc502286d4dcadefd911e
ChampionAsh5357 <championash5357@gmail.com>
2020-07-22 16:42:34 -0400

    Recipe

--- src/main/java/io/github/championash5357/tutorial/common/tags/TutorialTags.java
+++ src/main/java/io/github/championash5357/tutorial/common/tags/TutorialTags.java
@@ -6,26 +6,37 @@ import net.minecraft.item.Item;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ITag;
 import net.minecraft.tags.ItemTags;
+import net.minecraftforge.versions.forge.ForgeVersion;
 
 public class TutorialTags {
 
 	public static class Blocks {
 		
-		public static final ITag.INamedTag<Block> ORES_RUBY = tag("ores/ruby");
+		public static final ITag.INamedTag<Block> ORES_RUBY = forge("ores/ruby");
 		
+		@SuppressWarnings("unused")
 		private static ITag.INamedTag<Block> tag(String id) {
 			return BlockTags.makeWrapperTag(Tutorial.ID + ":" + id);
+		}
+		
+		private static ITag.INamedTag<Block> forge(String id) {
+			return BlockTags.makeWrapperTag(ForgeVersion.MOD_ID + ":" + id);
 		}
 	}
 
 	public static class Items {
 		
-		public static final ITag.INamedTag<Item> GEMS_RUBY = tag("gems/ruby");
-		public static final ITag.INamedTag<Item> ORES_RUBY = tag("ores/ruby");
-		public static final ITag.INamedTag<Item> CONTAINER_WATER = tag("container/water");
+		public static final ITag.INamedTag<Item> GEMS_RUBY = forge("gems/ruby");
+		public static final ITag.INamedTag<Item> ORES_RUBY = forge("ores/ruby");
+		public static final ITag.INamedTag<Item> CONTAINER_WATER = forge("container/water");
 		
+		@SuppressWarnings("unused")
 		private static ITag.INamedTag<Item> tag(String id) {
 			return ItemTags.makeWrapperTag(Tutorial.ID + ":" + id);
+		}
+		
+		private static ITag.INamedTag<Item> forge(String id) {
+			return ItemTags.makeWrapperTag(ForgeVersion.MOD_ID + ":" + id);
 		}
 	}
 }
